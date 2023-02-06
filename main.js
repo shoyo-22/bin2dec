1f0aeeacf119b20a3dd132ff3af26f23ecb67228
Yessen Murat <shoyo2209@gmail.com>
2023-02-06 14:20:11 +0600

    Basic algorithm for converting binary number to decimal

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Project Start");
+const convertForm = document.getElementById("convert-form");
+const binaryNumber = document.getElementById("binary");
+const resultSection = document.getElementById("result");
+
+convertForm.onsubmit = (event) => {
+  event.preventDefault();
+
+  const binaryNumberLength = binaryNumber.value.length;
+  let result = 0;
+
+  for (let i = 0; i < binaryNumberLength; i++) {
+    result += parseInt(binaryNumber.value[i]) * Math.pow(2, binaryNumberLength - (i + 1));
+  }
+  
+  resultSection.textContent = `Result: ${result}`;
+}
