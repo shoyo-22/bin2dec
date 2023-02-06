0cdd8ca3fdc42c796b27692172c71e9ef83b0cb6
Yessen Murat <shoyo2209@gmail.com>
2023-02-07 00:56:23 +0600

    Show solution in footer section

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 const convertForm = document.getElementById("convert-form");
 const binaryNumber = document.getElementById("binary");
 const resultSection = document.getElementById("result");
+const footer = document.querySelector("footer");
+const solutionSection = document.getElementById("solution");
 const clearBtn = document.getElementById("clear");
-let solved = false;
 
 convertForm.onsubmit = (event) => {
   event.preventDefault();
 
   let result = binToDecCalculate(binaryNumber.value);
-  solved = true;
 
+  footer.classList.remove("display-none");
   resultSection.textContent = `Result: ${result}`;
+  showSolution(binaryNumber.value, result);
 };
 
 clearBtn.onclick = () => {
   binaryNumber.value = "";
-}
+  footer.classList.add("display-none");
+};
 
 function binToDecCalculate(binary) {
   let decimal = 0;
-
   const binaryLength = binary.length;
 
   for (let i = 0; i < binaryLength; i++) {
@@ -27,4 +29,19 @@ function binToDecCalculate(binary) {
   }
 
   return decimal;
+}
+
+function showSolution(input, output) {
+  let solution = "";
+
+  for (let i = 0; i < input.length; i++) {
+    solution += `${input[i]}&times;2<sup>${input.length - i}</sup>`;
+
+    if (i !== input.length - 1) {
+      solution += " + ";
+    }
+  }
+
+  solutionSection.innerHTML = 
+    `<strong>Solution:</strong> <br />${input}<sub>2</sub> = ${solution} = ${output}<sub>10</sub>`;
 }
