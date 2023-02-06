8a86977f7efef3ffe9dd9e438bedd8bce9fe057a
Yessen Murat <shoyo2209@gmail.com>
2023-02-06 19:46:47 +0600

    Calculation part moved to function and created clear button

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 const convertForm = document.getElementById("convert-form");
 const binaryNumber = document.getElementById("binary");
 const resultSection = document.getElementById("result");
+const clearBtn = document.getElementById("clear");
+let solved = false;
 
 convertForm.onsubmit = (event) => {
   event.preventDefault();
 
-  const binaryNumberLength = binaryNumber.value.length;
-  let result = 0;
+  let result = binToDecCalculate(binaryNumber.value);
+  solved = true;
 
-  for (let i = 0; i < binaryNumberLength; i++) {
-    result += parseInt(binaryNumber.value[i]) * Math.pow(2, binaryNumberLength - (i + 1));
-  }
-  
   resultSection.textContent = `Result: ${result}`;
+};
+
+clearBtn.onclick = () => {
+  binaryNumber.value = "";
+}
+
+function binToDecCalculate(binary) {
+  let decimal = 0;
+
+  const binaryLength = binary.length;
+
+  for (let i = 0; i < binaryLength; i++) {
+    decimal += parseInt(binary[i]) * Math.pow(2, binaryLength - (i + 1));
+  }
+
+  return decimal;
 }
